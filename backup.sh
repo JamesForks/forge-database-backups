000f72c00d061c150231d169de623265c2dba2cc
James Brooks <james@alt-three.com>
2020-11-06 16:35:39 +0000

    wip

--- backup.sh
+++ backup.sh
@@ -14,55 +14,76 @@ mkdir -p /tmp/forge-backups/$BACKUP_ID
 
 # Run The Correct Backup Script For Each Database Driver
 
-if [[ $SERVER_DATABASE_DRIVER == 'mysql' ]]
+if [[ -z "$BACKUP_SHOULD_STREAM" ]]
 then
+    echo "Using original backup mechanism..."
 
-    for DATABASE in $BACKUP_DATABASES; do
-        mysqldump \
-            --user=root \
-            --password=$SERVER_DATABASE_PASSWORD \
-            --single-transaction \
-            $DATABASE > /tmp/forge-backups/$BACKUP_ID/$DATABASE.sql
-    done
-
-elif [[ $SERVER_DATABASE_DRIVER == 'pgsql' ]]
-then
-    for DATABASE in $BACKUP_DATABASES; do
-        sudo -u postgres pg_dump --clean -F p $DATABASE > /tmp/forge-backups/$BACKUP_ID/$DATABASE.sql
-    done
-fi
-
-# Add SQL Dump To Archive And Remove It Afterwards
-
-tar -czvf $BACKUP_ARCHIVE --remove-files -C /tmp/forge-backups/$BACKUP_ID/ .
-
-# Remove The Temp Directory
-
-rm -rf /tmp/forge-backups/$BACKUP_ID
-
-# Upload The Archived File
-
-if [ -f $BACKUP_ARCHIVE ]
-then
-    echo "Uploading backup archive..."
-
-    aws s3 cp /tmp/$BACKUP_ARCHIVE $BACKUP_FULL_STORAGE_PATH \
-        --profile=$BACKUP_AWS_PROFILE_NAME \
-        ${BACKUP_AWS_ENDPOINT:+ --endpoint=$BACKUP_AWS_ENDPOINT}
-
-    # Set A Failed Status
-
-    if [ $? -ne 0 ]
+    if [[ $SERVER_DATABASE_DRIVER == 'mysql' ]]
     then
-        echo "There was an error uploading the backup archive..."
-        BACKUP_STATUS=1
+        for DATABASE in $BACKUP_DATABASES; do
+            mysqldump \
+                --user=root \
+                --password=$SERVER_DATABASE_PASSWORD \
+                --single-transaction \
+                $DATABASE > /tmp/forge-backups/$BACKUP_ID/$DATABASE.sql
+        done
+
+    elif [[ $SERVER_DATABASE_DRIVER == 'pgsql' ]]
+    then
+        for DATABASE in $BACKUP_DATABASES; do
+            sudo -u postgres pg_dump --clean -F p $DATABASE > /tmp/forge-backups/$BACKUP_ID/$DATABASE.sql
+        done
     fi
 
-    # Remove Backup Archive
+    # Add SQL Dump To Archive And Remove It Afterwards
 
-    rm -f $BACKUP_ARCHIVE
+    tar -czvf $BACKUP_ARCHIVE --remove-files -C /tmp/forge-backups/$BACKUP_ID/ .
+
+    # Remove The Temp Directory
+
+    rm -rf /tmp/forge-backups/$BACKUP_ID
+
+    # Upload The Archived File
+
+    if [ -f $BACKUP_ARCHIVE ]
+    then
+        echo "Uploading backup archive..."
+
+        aws s3 cp /tmp/$BACKUP_ARCHIVE $BACKUP_FULL_STORAGE_PATH \
+            --profile=$BACKUP_AWS_PROFILE_NAME \
+            ${BACKUP_AWS_ENDPOINT:+ --endpoint=$BACKUP_AWS_ENDPOINT}
+
+        # Set A Failed Status
+
+        if [ $? -ne 0 ]
+        then
+            echo "There was an error uploading the backup archive..."
+            BACKUP_STATUS=1
+        fi
+
+        # Remove Backup Archive
+
+        rm -f $BACKUP_ARCHIVE
+    else
+        echo "Backup archive could not be created..."
+    fi
+
+    exit $BACKUP_STATUS
 else
-    echo "Backup archive could not be created..."
-fi
+    echo "Using streaming backup mechanism..."
 
-exit $BACKUP_STATUS
+    if [[ $SERVER_DATABASE_DRIVER == 'mysql' ]]
+    then
+        for DATABASE in $BACKUP_DATABASES; do
+            mysqldump \
+                --user=root \
+                --password=$SERVER_DATABASE_PASSWORD \
+                --single-transaction \
+                $DATABASE | \
+                tar chof - | \
+                aws s3 cp - $BACKUP_FULL_STORAGE_PATH \
+                --profile=$BACKUP_AWS_PROFILE_NAME \
+                ${BACKUP_AWS_ENDPOINT:+ --endpoint=$BACKUP_AWS_ENDPOINT}
+        done
+    fi
+fi
