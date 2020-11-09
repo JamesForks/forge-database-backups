84e2d24037dbb0549bb75c40d31078f65989c3a2
James Brooks <james@alt-three.com>
2020-11-09 10:06:33 +0000

    Fix archiving

--- backup.sh
+++ backup.sh
@@ -77,15 +77,14 @@ else
     if [[ $SERVER_DATABASE_DRIVER == 'mysql' ]]
     then
         for DATABASE in $BACKUP_DATABASES; do
-            BACKUP_ARCHIVE="$DATABASE.tar.gz"
+            BACKUP_ARCHIVE="$DATABASE.sql.gz"
 
             mysqldump \
                 --user=root \
                 --password=$SERVER_DATABASE_PASSWORD \
                 --single-transaction \
                 $DATABASE | \
-                # tar cf - $DATABASE | \
-                gzip -9 > $BACKUP_ARCHIVE | \
+                gzip -c | \
                 aws s3 cp - "$BACKUP_FULL_STORAGE_PATH$BACKUP_TIMESTAMP/$BACKUP_ARCHIVE" \
                 --profile=$BACKUP_AWS_PROFILE_NAME \
                 ${BACKUP_AWS_ENDPOINT:+ --endpoint=$BACKUP_AWS_ENDPOINT}
